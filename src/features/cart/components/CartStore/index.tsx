a5e150db94273adaa693b9f8469f669dfc4c8b26
DatNguyen <nguyentiendat39000@gmail.com>
2023-10-01 23:46:46 +0700

    update feature payment

--- src/features/cart/components/CartStore/index.tsx
+++ src/features/cart/components/CartStore/index.tsx
@@ -5,12 +5,60 @@ import { DeleteIcon } from '~/components/Svg';
 import Image from '~/components/Image';
 import { itemInCartType } from '~/types/cart';
 import { FormatPrice } from '~/utils/formatPrice';
+import { useAppDispatch } from '~/hooks';
+import { removeCartItem, setQuantity } from '~/slices/cartSlice';
+import { ChangeEvent, KeyboardEvent, SyntheticEvent, useState } from 'react';
 
 export interface CartStoreProps {
     data: itemInCartType[] | [];
 }
 export default function CartStore({ data }: CartStoreProps) {
     const cx = classNames.bind(style);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const dispatch = useAppDispatch();
+    const handleRemoveItem = (id: number) => {
+        dispatch(removeCartItem(id));
+    };
+    const handleIncreaseQuantity = (id: number, quantity: number, cartItem: itemInCartType) => {
+        dispatch(setQuantity({ newQuantity: quantity + 1, id }));
+        setTotalPrice((prev) => {
+            return prev + (cartItem.item?.salePrice || 1) * 1;
+        });
+    };
+    const handleDecreaseQuantity = (id: number, quantity: number, cartItem: itemInCartType) => {
+        if (quantity <= 1) {
+            setTotalPrice((prev) => {
+                return prev;
+            });
+            quantity = 2;
+        } else {
+            setTotalPrice((prev) => {
+                return prev - (cartItem.item?.salePrice || 1) * 1;
+            });
+        }
+        dispatch(setQuantity({ newQuantity: quantity - 1, id }));
+    };
+    const handleSetQuantity = (e: ChangeEvent<HTMLInputElement>, id: number) => {
+        const quantity = parseInt(e.target.value);
+        dispatch(setQuantity({ newQuantity: quantity, id }));
+    };
+    const handleNotNull = (e: KeyboardEvent<HTMLInputElement>, quantity: number) => {
+        if (e.key === 'Backspace' && quantity <= 9) {
+            e.preventDefault();
+        }
+    };
+    const handleCheckPayment = (e: SyntheticEvent, cartItem: itemInCartType) => {
+        const element = e.target as HTMLInputElement;
+        if (element.checked) {
+            setTotalPrice((prev) => {
+                return prev + (cartItem.item?.salePrice || 1) * cartItem.quantity;
+            });
+        } else if (!element.checked) {
+            setTotalPrice((prev) => {
+                return prev - (cartItem.item?.salePrice || 1) * cartItem.quantity;
+            });
+        }
+    };
     return (
         <div className={cx('container')}>
             <div className={cx('content-left')}>
@@ -19,7 +67,9 @@ export default function CartStore({ data }: CartStoreProps) {
                         <FormControlLabel
                             sx={{ fontSize: '1.2rem' }}
                             control={<Checkbox name="removeAll" />}
-                            label={<span className={cx('text')}>Tất cả (2 sản phẩm)</span>}
+                            label={
+                                <span className={cx('text')}>Tất cả ({data.length} sản phẩm)</span>
+                            }
                         />
                     </div>
                     <span>Đơn giá</span>
@@ -40,6 +90,9 @@ export default function CartStore({ data }: CartStoreProps) {
                                                 <FormControlLabel
                                                     sx={{ fontSize: '1.2rem' }}
                                                     control={<Checkbox name="removeItem" />}
+                                                    onChange={(e) => {
+                                                        handleCheckPayment(e, cartItem);
+                                                    }}
                                                     label={
                                                         <span className={cx('text')}>
                                                             {cartItem.item?.name}
@@ -61,18 +114,42 @@ export default function CartStore({ data }: CartStoreProps) {
                                         </div>
                                         <div className={cx('col-3')}>
                                             <div className={cx('form-wrapper')}>
-                                                <span className={cx('btn-decrease', 'btn')}>
+                                                <span
+                                                    onClick={() => {
+                                                        handleDecreaseQuantity(
+                                                            cartItem.item?.id || 1,
+                                                            cartItem.quantity,
+                                                            cartItem,
+                                                        );
+                                                    }}
+                                                    className={cx('btn-decrease', 'btn')}
+                                                >
                                                     <Image src="https://frontend.tikicdn.com/_desktop-next/static/img/icons/decrease.svg" />
                                                 </span>
                                                 <input
                                                     type="tel"
                                                     value={cartItem.quantity}
-                                                    onChange={() => {
-                                                        console.log(1);
+                                                    onChange={(e) => {
+                                                        handleSetQuantity(
+                                                            e,
+                                                            cartItem.item?.id || 1,
+                                                        );
+                                                    }}
+                                                    onKeyDown={(e) => {
+                                                        handleNotNull(e, cartItem.quantity);
                                                     }}
                                                     className={cx('input')}
                                                 />
-                                                <span className={cx('btn-increase', 'btn')}>
+                                                <span
+                                                    onClick={() => {
+                                                        handleIncreaseQuantity(
+                                                            cartItem.item?.id || 1,
+                                                            cartItem.quantity,
+                                                            cartItem,
+                                                        );
+                                                    }}
+                                                    className={cx('btn-increase', 'btn')}
+                                                >
                                                     <Image src="https://frontend.tikicdn.com/_desktop-next/static/img/icons/increase.svg" />
                                                 </span>
                                             </div>
@@ -83,7 +160,12 @@ export default function CartStore({ data }: CartStoreProps) {
                                             </span>
                                         </div>
                                         <div className={cx('col-5')}>
-                                            <span className={cx('delete-wrapper')}>
+                                            <span
+                                                onClick={() => {
+                                                    handleRemoveItem(cartItem.item?.id || 1);
+                                                }}
+                                                className={cx('delete-wrapper')}
+                                            >
                                                 <DeleteIcon className={cx('')} />
                                             </span>
                                         </div>
@@ -99,7 +181,7 @@ export default function CartStore({ data }: CartStoreProps) {
                     <div className={cx('prices-items')}>
                         <div className={cx('prices-item')}>
                             <div className={cx('prices-text')}>Tạm tính</div>
-                            <div className={cx('prices-value')}>0đ</div>
+                            <div className={cx('prices-value')}>{FormatPrice(totalPrice)}đ</div>
                         </div>
                         <div className={cx('prices-item')}>
                             <div className={cx('prices-text')}>Giảm giá</div>
--- src/slices/cartSlice.ts
+++ src/slices/cartSlice.ts
@@ -25,9 +25,21 @@ const cartSlice = createSlice({
         addToCart(state, action: PayloadAction<itemInCartType>) {
             state.listCart = [...state.listCart, { ...action.payload }];
         },
+        setQuantity(state, action: PayloadAction<{ newQuantity: number; id: number }>) {
+            const newListCart = state.listCart.map((cartItem) => {
+                if (cartItem.item?.id === action.payload.id) {
+                    return { ...cartItem, quantity: action.payload.newQuantity };
+                }
+                return { ...cartItem };
+            });
+            state.listCart = [...newListCart];
+        },
+        removeCartItem(state, action: PayloadAction<number>) {
+            state.listCart = state.listCart.filter((item) => item.item?.id !== action.payload);
+        },
     },
 });
 
-export const { showTippy, hideTippy, addToCart } = cartSlice.actions;
+export const { showTippy, hideTippy, addToCart, removeCartItem, setQuantity } = cartSlice.actions;
 const cartReducer = cartSlice.reducer;
 export default cartReducer;
