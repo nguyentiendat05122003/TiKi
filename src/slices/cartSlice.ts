b3d1e9bb709f613be6170232318c78b6d6b8dd61
DatNguyen <nguyentiendat39000@gmail.com>
2023-10-03 14:42:23 +0700

    fix cart

--- src/slices/cartSlice.ts
+++ src/slices/cartSlice.ts
@@ -23,7 +23,23 @@ const cartSlice = createSlice({
             state.isShowTippyCart = false;
         },
         addToCart(state, action: PayloadAction<itemInCartType>) {
-            state.listCart = [...state.listCart, { ...action.payload }];
+            //state.listCart = [...state.listCart, action.payload];
+            const newList = [...state.listCart, action.payload];
+            const listItemRecur = newList.filter(
+                (cartItem) => cartItem.item?.id === action.payload.item?.id,
+            );
+            const listItemNotRecur = newList.filter(
+                (cartItem) => cartItem.item?.id !== action.payload.item?.id,
+            );
+            if (listItemRecur.length === 0) {
+                state.listCart = [...state.listCart, action.payload];
+            } else {
+                const totalQuantity = listItemRecur.reduce((results, cur) => {
+                    return results + cur.quantity;
+                }, 0);
+                const newItem = [{ item: listItemRecur[0].item, quantity: totalQuantity }];
+                state.listCart = newItem.concat(listItemNotRecur);
+            }
             localStorage.setItem('listCart', JSON.stringify(state.listCart));
         },
         setQuantity(state, action: PayloadAction<{ newQuantity: number; id: number }>) {
